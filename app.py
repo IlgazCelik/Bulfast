c506f21f3f714d7c5c019a0389da19421a597f67
Faruk Ilgaz Celik <71097904+IlgazCelik@users.noreply.github.com>
2026-05-16 23:36:30 +0200

    Added search function

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-import model
-import flask
+from model import findClosestNeighbors
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+vectorSize = 512
+
+
+
+#Search function
+@app.route("/search", methods=["POST"])
+def search():
+    data = request.get_json() or {}
+    vectorStn = [0.0] * vectorSize
+    user_vector = data.get("vector", vectorStn)
+    threshold = data.get("threshold", 0.1)
+
+    matched_indices = findClosestNeighbors(user_vector, DATABASE, threshold)
+
+    return jsonify({"success": True, "matchesIdx": matched_indices})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 #To do
-#Crate and connect html templates
-#Connect templates with model
-#Add login and etc.
+#Integrate database operations
